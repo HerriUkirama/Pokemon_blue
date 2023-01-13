21927f79ca0465c6c08049a35169d7a9ee4cfd8c
Herri Purba <119906399+HerriUkirama@users.noreply.github.com>
2023-01-13 16:57:25 +0700

    new pokomon page

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,6 +1,20 @@
 class PokemonsController < ApplicationController
     def index
-        
     end
+    
+    def new
+        @pokemon = Pokemon.new
+    end
+
+    def create
+        @pokemon = Pokemon.new(name: params[:name])
+        
+        if @pokemon.save
+            redirect_to pokemons_path
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+    
     
 end
